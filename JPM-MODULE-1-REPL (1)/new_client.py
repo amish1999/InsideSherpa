82d7eefaabf25cb1f1bd08b66edbd129c1997797
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2020-05-22 20:25:26 +0530

    Add files via upload

---
+++ JPM-MODULE-1-REPL (1)/new_client.py
@@ -0,0 +1,10 @@
+#This is where you would manually type the existing code in the client.py, which is inside the jpm_module_1 folder (disregard copying the useless comments) so that you can avoid the dreaded tab editing issues in REPL particularly for the Python environment only
+
+#After manually copying the useful code in client.py, you can make your changes here
+
+#Before you test your modified code is working, make sure you have done the initial steps needed to be done before making changes i.e. Part V in the Instructions file
+
+#Then, to test, first, delete the client.py file inside jpm_module_1. Then rename this file to just client.py. Finally move this file inside the jpm_module_1 folder  
+
+# Delete these comments when you're done...
+# Comments are anything that's preceded with '#'
